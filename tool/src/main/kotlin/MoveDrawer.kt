f452cb562c883ee8f3a5167c88206f30c396e2b9
andreysoldatenkow <andreysoldatenkow@gmail.com>
2021-06-17 03:59:42 +0300

    add Animator and PieceOnBoardView

--- tool/src/main/kotlin/MoveDrawer.kt
+++ tool/src/main/kotlin/MoveDrawer.kt
@@ -51,12 +51,22 @@ class JFrameMoveDrawer(private val width: Int, val height: Int, val fps: Int) : 
 
     private fun drawMoves(board: Board, moves: List<Move>, isKingAttacked: Boolean = false) {
         if (frameListener == null) return
+        println("Rendered move ${moves.joinToString()}}")
         val movedPieces = mutableListOf<Pair<Piece, Move>>()
         var kingSide: Side? = null
+        var kingWhite: Cell? = null
+        var kingBlack: Cell? = null
         val boardWithoutMovedPiece = board.copy(
             cells = board.cells.map { cellWithPiece ->
                 if (isKingAttacked && cellWithPiece.cell == moves.first().from) {
                     kingSide = cellWithPiece.piece!!.side.opposite()
+                }
+                if (cellWithPiece.piece != null && cellWithPiece.piece.pieceName == PieceName.King) {
+                    if (cellWithPiece.piece.side == Side.Black) {
+                        kingBlack = cellWithPiece.cell
+                    } else {
+                        kingWhite = cellWithPiece.cell
+                    }
                 }
                 val move: Move? = moves.find { cellWithPiece.cell == it.from }
                 if (move != null) {
@@ -69,25 +79,62 @@ class JFrameMoveDrawer(private val width: Int, val height: Int, val fps: Int) : 
                 }
             }
         )
-        val image = drawBoardWithPieces(boardWithoutMovedPiece, kingSide)
-        val frameCount = MOVE_DURATION / frameDuration
-        println("Rendered move ${moves.joinToString()}}")
-        for (ix in 0..frameCount) {
-            val frame = BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR)
-            val g = frame.graphics
-            g.drawImage(image, 0, 0, null)
-            movedPieces.forEach { (movedPiece, move) ->
-                val pieceIcon = movedPiece.icon()
-                val leftTopFromX = move.from.x * cellSize + topLeftBoardX
-                val leftTopFromY = (7 - move.from.y) * cellSize + topLeftBoardY
-                val leftTopToX = move.to.x * cellSize + topLeftBoardX
-                val leftTopToY = (7 - move.to.y) * cellSize + topLeftBoardY
-                val dx = (leftTopToX - leftTopFromX) / frameCount
-                val dy = (leftTopToY - leftTopFromY) / frameCount
-                g.drawImage(pieceIcon, leftTopFromX + dx * ix, leftTopFromY + dy * ix, cellSize, cellSize, null)
+        val boardWithPiecesView = PieceOnBoardView(
+            topLeftBoardX,
+            topLeftBoardY,
+            cellSize,
+            cellSize,
+            boardWithoutMovedPiece
+        )
+
+        val boardWithPieces = BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR)
+        val boardWithPiecesGraphics = boardWithPieces.graphics
+        boardWithPiecesGraphics.drawImage(cleanBoard, 0, 0, null)
+        if (kingSide != null) {
+            val cellView = if (kingSide == Side.Black) {
+                CellView(
+                    kingBlack!!.x * cellSize + topLeftBoardX,
+                    kingBlack!!.y * cellSize + topLeftBoardY,
+                    cellSize,
+                    cellSize,
+                    redColor
+                )
+            } else {
+                CellView(
+                    kingWhite!!.x * cellSize + topLeftBoardX,
+                    kingWhite!!.y * cellSize + topLeftBoardY,
+                    cellSize,
+                    cellSize,
+                    redColor
+                )
             }
-            frameListener?.invoke(frame)
+            cellView.draw(graphics = boardWithPiecesGraphics)
         }
+        boardWithPiecesView.draw(graphics = boardWithPiecesGraphics)
+        val movedPiece = movedPieces.map {
+            PieceView(
+                it.second.from.x * cellSize + topLeftBoardX,
+                it.second.from.y * cellSize + topLeftBoardY,
+                cellSize,
+                cellSize,
+                it.first
+            )
+        }
+
+        val animatorPieceMove = AnimatorPieceMove(
+            pieceViews = movedPiece,
+            interpolator = InterpolatorCube(),
+            duration = MOVE_DURATION,
+            frameListener = frameListener!!,
+            coordinatesTo = movedPieces.map {
+                Pair(
+                    it.second.to.x * cellSize + topLeftBoardX,
+                    it.second.to.y * cellSize + topLeftBoardY
+                )
+            }
+        )
+        boardWithPiecesGraphics.drawImage(boardWithPieces, 0, 0, null)
+        animatorPieceMove.startAnimator(boardWithPieces)
     }
 
     private fun drawBoardWithPieces(boardCurrent: Board, kingAttackedSide: Side?): BufferedImage {
@@ -95,7 +142,6 @@ class JFrameMoveDrawer(private val width: Int, val height: Int, val fps: Int) : 
         val g = boardWithPieces.graphics
         g.drawImage(cleanBoard, 0, 0, null)
         boardCurrent.cells.forEach {
-
             if (it.piece != null) {
                 val pieceBufferedImage = it.piece.icon()
                 val y = 7 - it.cell.y
@@ -198,6 +244,84 @@ interface MovableView<T : View> {
     fun move(x: Int, y: Int): T
 }
 
+interface Animator {
+    val pieceViews: List<PieceView>
+    val coordinatesTo: List<Pair<Int, Int>>
+    val interpolator: Interpolator
+    val duration: Int
+    val frameListener: (BufferedImage) -> Unit
+
+    fun startAnimator(bufferedImage: BufferedImage)
+}
+
+interface Interpolator {
+    fun getInterpolation(t: Float): Float
+}
+
+class AnimatorPieceMove(
+    override val pieceViews: List<PieceView>,
+    override val coordinatesTo: List<Pair<Int, Int>>,
+    override val interpolator: Interpolator,
+    override val duration: Int,
+    override val frameListener: (BufferedImage) -> Unit
+) : Animator {
+    override fun startAnimator(bufferedImage: BufferedImage) {
+        val frameCount = (duration / 16)
+        for (ix in 0..frameCount) {
+            val frame = BufferedImage(bufferedImage.width, bufferedImage.height, BufferedImage.TYPE_3BYTE_BGR)
+            val g = frame.graphics
+            pieceViews.forEachIndexed { pieceViewsIx, it ->
+                g.drawImage(bufferedImage, 0, 0, null)
+                val dx =
+                    ((coordinatesTo[pieceViewsIx].first - it.x) * interpolator.getInterpolation(ix / frameCount.toFloat()))
+                        .toInt()
+                val dy =
+                    ((coordinatesTo[pieceViewsIx].second - it.y) * interpolator.getInterpolation(ix / frameCount.toFloat()))
+                        .toInt()
+                it.move(it.x + dx, it.y + dy).draw(g)
+            }
+            frameListener.invoke(frame)
+        }
+    }
+}
+
+class InterpolatorCube : Interpolator {
+    override fun getInterpolation(t: Float): Float {
+        return t * t * t
+    }
+}
+
+
+class PieceOnBoardView(
+    override val x: Int,
+    override val y: Int,
+    override val width: Int,
+    override val height: Int,
+    private val board: Board
+) : View {
+
+    private val pieceViews by lazy { views() }
+
+    override fun draw(graphics: Graphics) {
+        pieceViews.forEach { it.draw(graphics) }
+    }
+
+    private fun views(): List<PieceView> {
+        return board.cells
+            .filter { it.piece != null }
+            .map {
+                PieceView(
+                    it.cell.x * width + x,
+                    it.cell.y * height + y,
+                    width,
+                    height,
+                    it.piece!!
+                )
+            }
+    }
+}
+
+
 class BoardView(
     override val x: Int,
     override val y: Int,
@@ -237,7 +361,7 @@ class BoardView(
     }
 }
 
-class PieceView (
+class PieceView(
     override val x: Int,
     override val y: Int,
     override val width: Int,
