7e4fe54dbfcdfc0e7bed243404dd8f2f3af8e7fc
andreysoldatenkow <andreysoldatenkow@gmail.com>
2021-06-18 02:24:09 +0300

    fix size and isRendered flag

--- tool/src/main/kotlin/io/chessy/tool/view/OnceDrawView.kt
+++ tool/src/main/kotlin/io/chessy/tool/view/OnceDrawView.kt
@@ -5,12 +5,14 @@ import java.awt.image.BufferedImage
 
 class OnceDrawView(private val wrappedView: View) : View by wrappedView {
 
-    private val buffer by lazy { BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR) }
+    private val buffer by lazy { BufferedImage(1080, 1920, BufferedImage.TYPE_3BYTE_BGR) }
     private var isRendered = false
+
 
     override fun draw(graphics: Graphics) {
         if (!isRendered) {
             wrappedView.draw(buffer.graphics)
+            isRendered = true
         }
         graphics.drawImage(
             buffer,
